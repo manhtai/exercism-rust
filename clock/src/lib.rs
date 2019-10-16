95b451edb6557bf01765cf7ef6629dd7ec15c466
Mạnh Tài <vo@manhtai.com>
2019-10-16 16:03:04 +0700

    Make clock shorter

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,5 +1,6 @@
-use core::{fmt, cmp};
+use core::fmt;
 
+#[derive(Debug, PartialEq)]
 pub struct Clock {
     hours: i32,
     minutes: i32,
@@ -7,44 +8,18 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut m = minutes % 60;
-        let mut h = minutes / 60;
-
-        if m < 0 {
-            m += 60;
-            h -= 1;
-        };
-
-        h = (hours + h) % 24;
-        if (h < 0) {
-            h = h + 24;
-        }
+        let m = minutes.rem_euclid(60);
+        let h = (minutes.div_euclid(60) + hours).rem_euclid(24);
         Clock { hours: h, minutes: m }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let m = &self.minutes + minutes;
-        let h = m / 60;
-        let minutes = m % 60;
-        let hours = (&self.hours + h) % 24;
-        Clock::new(hours, minutes)
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:02}:{:02}", self.hours, self.minutes)
-    }
-}
-
-impl fmt::Debug for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
-    }
-}
-
-impl cmp::PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.hours == other.hours && self.minutes == other.minutes
     }
 }
