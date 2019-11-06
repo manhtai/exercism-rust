08869438779026eaa81398601a3940d64a785a02
manhtai <vo@manhtai.com>
2019-11-06 20:32:27 +0700

    Remove const

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,7 +1,5 @@
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
 const CIPHER: &str = "zyxwvutsrqponmlkjihgfedcba";
-const a: u32 = 'a' as u32;
-const z: u32 = 'z' as u32;
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
@@ -15,7 +13,7 @@ pub fn encode(plain: &str) -> String {
 
         let c_lower = c.to_ascii_lowercase();
         if c_lower >= 'a' && c_lower <= 'z' {
-            let range = (c_lower as u32 - a) as usize;
+            let range = (c_lower as u8 - b'a') as usize;
             cipher.push_str(&CIPHER[range..range + 1]);
             count += 1;
         } else if c_lower >= '0' && c_lower <= '9' {
@@ -32,7 +30,7 @@ pub fn decode(cipher: &str) -> String {
     for c in cipher.chars() {
         let c_lower = c.to_ascii_lowercase();
         if c_lower >= 'a' && c_lower <= 'z' {
-            let range = (z - c_lower as u32) as usize;
+            let range = (b'z' - c_lower as u8) as usize;
             plain.push_str(&ALPHABET[range..range + 1]);
         } else if c_lower > '0' && c_lower <= '9' {
             plain.push_str(&c_lower.to_string());
