904949b0f3b1004fe643810a97992990e4de666b
manhtai <v.taivm1@vinid.net>
2019-11-07 09:45:01 +0700

    Refactor code

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,29 +1,27 @@
+fn translate(c: char) -> String {
+    let c_lower = c.to_ascii_lowercase();
+    return if c_lower >= 'a' && c_lower <= 'z' {
+        char::from(b'a' + b'z' - c_lower as u8).to_string()
+    } else if c_lower >= '0' && c_lower <= '9' {
+        c_lower.to_string()
+    } else {
+        "".to_owned()
+    };
+}
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let mut count = 1;
-    plain.chars().map(|c| { c.to_ascii_lowercase() }).map(|c_lower| {
-        let sep = if count % 5 == 0 { " " } else { "" };
-        return if c_lower >= 'a' && c_lower <= 'z' {
-            count += 1;
-            char::from(b'z' - c_lower as u8 + b'a').to_string() + sep
-        } else if c_lower >= '0' && c_lower <= '9' {
-            count += 1;
-            c_lower.to_string() + sep
-        } else {
-            "".to_owned()
-        };
-    }).collect::<String>().trim().to_string()
+    plain.chars()
+        .map(translate)
+        .filter(|c| c != "")
+        .collect::<Vec<String>>()
+        .chunks(5)
+        .map(|c| c.join(""))
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher.chars().map(|c| { c.to_ascii_lowercase() }).map(|c_lower| {
-        return if c_lower >= 'a' && c_lower <= 'z' {
-            char::from(b'a' + b'z' - c_lower as u8).to_string()
-        } else if c_lower > '0' && c_lower <= '9' {
-            c_lower.to_string()
-        } else {
-            "".to_owned()
-        };
-    }).collect()
+    cipher.chars().map(translate).collect()
 }
