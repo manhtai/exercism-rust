881728e18c9e195e0b90ce0af26474f1d4d1b123
manhtai <v.taivm1@vinid.net>
2019-11-07 11:15:11 +0700

    Optimize a bit

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,27 +1,26 @@
-fn translate(c: char) -> String {
+fn translate(c: char) -> Option<char> {
     let c_lower = c.to_ascii_lowercase();
     return if c_lower >= 'a' && c_lower <= 'z' {
-        char::from(b'a' + b'z' - c_lower as u8).to_string()
+        Some(char::from(b'a' + b'z' - c_lower as u8))
     } else if c_lower >= '0' && c_lower <= '9' {
-        c_lower.to_string()
+        Some(c_lower)
     } else {
-        "".to_owned()
+        None
     };
 }
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
     plain.chars()
-        .map(translate)
-        .filter(|c| c != "")
-        .collect::<Vec<String>>()
+        .filter_map(translate)
+        .collect::<Vec<char>>()
         .chunks(5)
-        .map(|c| c.join(""))
+        .map(|c| c.iter().collect::<String>())
         .collect::<Vec<String>>()
         .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher.chars().map(translate).collect()
+    cipher.chars().filter_map(translate).collect()
 }
