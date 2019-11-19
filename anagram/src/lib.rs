4e740c4b9893beb64ae9432a50ee892469201b25
manhtai <vo@manhtai.com>
2019-11-19 20:29:03 +0700

    Lowercase whole string

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,16 +1,20 @@
 use std::collections::HashSet;
 
 fn sort_word(word: &str) -> String {
-    let mut new_word = word.chars().map(|c| c.to_lowercase().to_string()).collect::<Vec<String>>();
+    let mut new_word = word
+        .to_lowercase()
+        .to_string()
+        .chars()
+        .collect::<Vec<char>>();
     new_word.sort();
-    return new_word.join("");
+    return new_word.iter().collect();
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let lowercase_word = word.to_lowercase().to_string();
+    let lowercase_word = word.to_lowercase();
     let sorted_word = sort_word(word);
     return possible_anagrams.iter()
-        .filter(|c| c.to_lowercase() != lowercase_word)
-        .filter_map(|c| if sort_word(c) == sorted_word { Some(c.to_owned()) } else { None })
+        .filter(|c| c.to_lowercase() != lowercase_word && sort_word(c) == sorted_word)
+        .cloned()
         .collect();
 }
