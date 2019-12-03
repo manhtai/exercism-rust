55d1ad3e394605b782ac954f6686e099903a950f
manhtai <vo@manhtai.com>
2019-12-03 20:34:31 +0700

    Use const instead of RY

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -15,7 +15,10 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64;
+    const RATE: f64;
+    fn years_during(d: &Duration) -> f64 {
+        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / Self::RATE
+    }
 }
 
 pub struct Mercury;
@@ -35,49 +38,33 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 0.2408467
-    }
+    const RATE: f64 = 0.2408467;
 }
 
 impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 0.61519726
-    }
+    const RATE: f64 = 0.61519726;
 }
 
 impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS
-    }
+    const RATE: f64 = 1.0;
 }
 
 impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 1.8808158
-    }
+    const RATE: f64 = 1.8808158;
 }
 
 impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 11.862615
-    }
+    const RATE: f64 = 11.862615;
 }
 
 impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 29.447498
-    }
+    const RATE: f64 = 29.447498;
 }
 
 impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 84.016846
-    }
+    const RATE: f64 = 84.016846;
 }
 
 impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / ONE_EARTH_YEAR_IN_SECONDS / 164.79132
-    }
+    const RATE: f64 = 164.79132;
 }
