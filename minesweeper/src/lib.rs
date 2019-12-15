4287000ca18ceceb6939c25c4cd51ca9f9d52109
manhtai <vo@manhtai.com>
2019-12-15 16:56:50 +0700

    Stupid answer

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
+    let list: Vec<Vec<char>> = minefield.iter().map(|x| x.chars().collect()).collect();
+    let mut result: Vec<Vec<u8>> = vec![vec![0; list[0].len()]; list.len()];
+    for (i, row) in list.iter().enumerate() {
+        for (j, c) in row.iter().enumerate() {
+            if c.to_owned() == '*' {
+                for m in (i as isize) - 1..=(i as isize) + 1 {
+                    for n in (j as isize) - 1..=(j as isize) + 1 {
+                        if m >= 0 && n >= 0 && m < list.len() as isize && n < row.len() as isize {
+                            result[m as usize][n as usize] += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    result.iter().enumerate().map(|(i, row)| {
+        row.iter().enumerate().map(|(j, c)| {
+            match list[i][j].to_owned() {
+                '*' => "*".to_owned(),
+                _ => if *c != 0 { c.to_string() } else { " ".to_owned() },
+            }
+        }).collect()
+    }).collect()
 }
