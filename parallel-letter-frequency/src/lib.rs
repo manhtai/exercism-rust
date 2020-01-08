26d9fd6cb8174b9577558f7c90eadee2ed374b29
manhtai <vo@manhtai.com>
2020-01-08 23:02:07 +0700

    Refactor code

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -5,9 +5,6 @@ use std::cmp;
 
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    // For default value
-    let zero: usize = 0;
-
     // Init concurrent vec
     let concurrent_vec = Arc::new(Mutex::new(vec![]));
     let mut children = vec![];
@@ -17,21 +14,34 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let running_worker = cmp::min(str_len, worker_count);
     let each_worker_load = str_len.div_euclid(running_worker);
 
+    // Convert char to map
+    let char_to_map = |c: char| {
+        let mut h = HashMap::new();
+        h.insert(c.to_ascii_lowercase(), 1);
+        h
+    };
+
+    // Join many maps into one
+    let fold_map = |a: HashMap<char, usize>, b: HashMap<char, usize>| {
+        let zero: usize = 0;
+        let mut c: HashMap<char, usize> = a.into_iter().collect();
+        for (k, v) in b.iter() {
+            c.insert(*k, c.get(k).unwrap_or(&zero) + v);
+        }
+        c
+    };
+
+
     for i in 0..running_worker {
         let clone_v = concurrent_vec.clone();
         let string = input[i * each_worker_load..cmp::min((i + 1) * each_worker_load, str_len)]
             .join("");
 
         children.push(thread::spawn(move || {
-            let mut h: HashMap<char, usize> = HashMap::new();
-            for c in string.chars() {
-                if !c.is_alphabetic() {
-                    continue;
-                }
-
-                let lc = c.to_ascii_lowercase();
-                h.insert(lc, h.get(&lc).unwrap_or(&zero) + 1);
-            }
+            let h = string.chars()
+                .filter(|c| c.is_alphabetic())
+                .map(char_to_map)
+                .fold(HashMap::new(), fold_map);
             clone_v.lock().unwrap().push(h);
         }));
     };
@@ -43,13 +53,5 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
 
     // Calculate final result
     let cv = concurrent_vec.lock().unwrap().clone();
-    cv
-        .into_iter()
-        .fold(HashMap::new(), |a, b| {
-            let mut c: HashMap<char, usize> = a.into_iter().collect();
-            for (k, v) in b.iter() {
-                c.insert(*k, c.get(k).unwrap_or(&zero) + v);
-            }
-            c
-        })
+    cv.into_iter().fold(HashMap::new(), fold_map)
 }
