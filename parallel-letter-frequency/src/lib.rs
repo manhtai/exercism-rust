cd2bbba8e76ecf6e462ff2d596fc66a2c2e88430
manhtai <vo@manhtai.com>
2020-01-09 20:30:12 +0700

    Copy the code

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -15,13 +15,6 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let running_worker = cmp::min(str_len, worker_count);
     let each_worker_load = str_len.div_euclid(running_worker);
 
-    // Convert char to map
-    let char_to_map = |c: char| {
-        let mut h = HashMap::new();
-        h.insert(c.to_ascii_lowercase(), 1);
-        h
-    };
-
     // Join many maps into one
     let fold_map = |a: HashMap<char, usize>, b: HashMap<char, usize>| {
         let zero: usize = 0;
@@ -38,11 +31,13 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
             input[i * each_worker_load..cmp::min((i + 1) * each_worker_load, str_len)].join("");
 
         children.push(thread::spawn(move || {
-            string
-                .chars()
-                .filter(|c| c.is_alphabetic())
-                .map(char_to_map)
-                .fold(HashMap::new(), fold_map)
+            let mut map = HashMap::new();
+            for chr in string.chars().filter(|c| c.is_alphabetic()) {
+                if let Some(c) = chr.to_lowercase().next() {
+                    (*map.entry(c).or_insert(0)) += 1;
+                }
+            }
+            map
         }));
     }
 
