ce31fb7baf3aa5bfb8a781b716e11d67f8b9fd37
manhtai <vo@manhtai.com>
2020-01-12 23:30:29 +0700

    Almost...

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -1,5 +1,5 @@
+use std::cmp::Reverse;
 use std::collections::{HashMap, HashSet};
-use std::cmp::Ordering;
 
 /// Given a list of poker hands, return a list of those hands which win.
 ///
@@ -7,30 +7,6 @@ use std::cmp::Ordering;
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
     find_all_winning_hands(hands)
-}
-
-#[derive(Eq)]
-struct Hand {
-    rank: usize,
-    key: String,
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.rank.cmp(&other.rank)
-    }
-}
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.rank.cmp(&other.rank))
-    }
-}
-
-impl PartialEq for Hand {
-    fn eq(&self, other: &Self) -> bool {
-        self.rank == other.rank
-    }
 }
 
 fn find_all_winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
@@ -42,12 +18,13 @@ fn find_all_winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
         let lt = if point > max_point {
             true
         } else if point == max_point {
-            rank.lt(&max_rank)
+            rank.gt(&max_rank)
         } else {
             false
         };
 
         let eq = point == max_point && rank.eq(&max_rank);
+
         if result.is_empty() || lt {
             result = vec![hand];
             max_point = point;
@@ -80,29 +57,37 @@ fn find_hand(h: &str) -> (&str, usize) {
 
 fn get_hand_rank(hand: &str) -> (i32, Vec<usize>) {
     let mut groups = HashMap::new();
-    let hands = hand
-        .to_string()
+    let keys = hand
         .split_whitespace()
         .map(|s| {
             let (key, rank) = find_hand(s);
             (*groups.entry(rank).or_insert(0)) += 1;
-            Hand { rank, key: key.to_owned() }
+            key
         })
-        .collect::<Vec<Hand>>();
+        .collect::<HashSet<&str>>();
 
-    let mut hands_vec = groups
-        .iter()
-        .collect::<Vec<(&usize, &i32)>>();
+    let mut hands_vec = groups.iter().collect::<Vec<(&usize, &i32)>>();
     hands_vec.sort();
 
-    let counts = hands_vec.iter().map(|(rank, count)| **count).collect::<Vec<i32>>();
-    let ranks = hands_vec.iter().map(|(rank, count)| **rank).collect::<Vec<usize>>();
+    let counts = hands_vec
+        .iter()
+        .map(|(_, count)| **count)
+        .collect::<Vec<i32>>();
 
-    if ranks.eq(&vec![14, 5, 4, 3, 2]) {
-        let ranks = vec![5, 4, 3, 2, 1];
-    }
-    let straight = ranks.len() == 5 && (ranks.iter().max().unwrap() - ranks.iter().min().unwrap()) == 4;
-    let flush = hands.iter().map(|h| h.key.to_owned()).collect::<HashSet<String>>().len() == 1;
+    let mut ranks = hands_vec
+        .iter()
+        .map(|(rank, _)| **rank)
+        .collect::<Vec<usize>>();
+
+    ranks = if ranks.eq(&vec![14, 5, 4, 3, 2]) {
+        vec![5, 4, 3, 2, 1]
+    } else {
+        ranks
+    };
+
+    let straight =
+        ranks.len() == 5 && (ranks.iter().max().unwrap() - ranks.iter().min().unwrap()) == 4;
+    let flush = keys.len() == 1;
 
     let sort = if counts.eq(&vec![5]) {
         9
