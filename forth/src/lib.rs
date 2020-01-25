0ee92dd495e909be40071ce7809f4900f06c5959
manhtai <vo@manhtai.com>
2020-01-25 12:03:11 +0700

    All tests passed

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -1,10 +1,12 @@
 use crate::Error::{UnknownWord, InvalidWord, StackUnderflow, DivisionByZero};
+use std::collections::HashMap;
 
 pub type Value = i32;
 pub type ForthResult = Result<(), Error>;
 
 pub struct Forth {
     stack: Vec<Value>,
+    env: HashMap<String, Vec<String>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -17,7 +19,7 @@ pub enum Error {
 
 impl Forth {
     pub fn new() -> Forth {
-        Forth { stack: vec![] }
+        Forth { stack: vec![], env: HashMap::new() }
     }
 
     pub fn stack(&self) -> Vec<Value> {
@@ -25,7 +27,28 @@ impl Forth {
     }
 
     pub fn eval(&mut self, input: &str) -> ForthResult {
-        let result = input.split_whitespace().map(|ops| {
+        let input_vec: Vec<String> = input.split_whitespace().map(|s| s.to_lowercase()).collect();
+
+        // Define words
+        if input_vec.starts_with(&[":".to_owned()]) {
+            if input_vec.len() < 4 {
+                return Err(InvalidWord);
+            }
+
+            let word = input_vec[1].to_owned();
+            let definition = input_vec[2..input_vec.len() - 1].to_vec();
+            if word.chars().any(|c| c.is_numeric()) {
+                return Err(InvalidWord);
+            }
+
+            self.env.insert(word, self.extend_def(definition));
+            return Ok(());
+        }
+
+        let extended_vec = self.extend_def(input_vec);
+
+        // Operator
+        let result = extended_vec.into_iter().map(|ops| {
             if ops.chars().all(|c| c.is_numeric()) {
                 let val = ops.parse::<Value>();
                 return match val {
@@ -34,10 +57,10 @@ impl Forth {
                         Ok(())
                     }
                     _ => Err(InvalidWord),
-                }
+                };
             }
 
-            match ops.to_lowercase().as_str() {
+            match ops.as_str() {
                 "+" | "-" | "*" | "/" | "swap" => {
                     let a_opt = self.stack.pop();
                     let b_opt = self.stack.pop();
@@ -48,7 +71,7 @@ impl Forth {
                     let a = a_opt.unwrap();
                     let b = b_opt.unwrap();
 
-                    match ops {
+                    match ops.as_str() {
                         "+" => self.stack.push(a + b),
                         "-" => self.stack.push(b - a),
                         "*" => self.stack.push(a * b),
@@ -58,6 +81,7 @@ impl Forth {
                             }
                             self.stack.push(b / a);
                         }
+                        // swap
                         _ => {
                             self.stack.push(a);
                             self.stack.push(b);
@@ -96,5 +120,19 @@ impl Forth {
             Some(e) => e,
             _ => Ok(())
         }
+    }
+
+    fn extend_def(&self, input_vec: Vec<String>) -> Vec<String> {
+        // Extend words
+        let mut extended_vec: Vec<String> = Vec::new();
+        for v in input_vec {
+            let key = v.as_str();
+            if self.env.contains_key(key) {
+                extended_vec.extend_from_slice(self.env.get(key).unwrap());
+            } else {
+                extended_vec.push(key.to_string());
+            }
+        }
+        extended_vec
     }
 }
